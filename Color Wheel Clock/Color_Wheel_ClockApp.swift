4a418fece13f8dadb862890b4752c9c81e03b3cf
Konner <rigbykonner@gmail.com>
2026-04-14 09:39:46 -0700

    Initial Commit

---
+++ Color Wheel Clock/Color_Wheel_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Wheel_ClockApp.swift
+//  Color Wheel Clock
+//
+//  Created by Konner Rigby on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Color_Wheel_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
